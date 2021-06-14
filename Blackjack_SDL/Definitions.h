0a0363afab266dbc124912dee71d7e12f2ccd974
pbrodie94 <82655776+pbrodie94@users.noreply.github.com>
2021-06-14 17:41:55 -0400

    Initial Commit

---
+++ Blackjack_SDL/Definitions.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <sdl.h>
+#include <sdl_image.h>
+#include <sdl_ttf.h>
+#include <sdl_mixer.h>
+
+using namespace std;
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
